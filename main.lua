c848b8bb2b4f50218683edd4d786d857aae4d274
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-07-29 17:22:58 -0500

    getting hello world and basic level stuff rendering was hard

--- main.lua
+++ main.lua
@@ -1,7 +1,24 @@
 require "debugger"
 require "prism"
 
+prism.loadModule("prism/spectrum")
+prism.loadModule("modules/game")
+
+local spriteAtlas = spectrum.SpriteAtlas.fromASCIIGrid("display/wanderlust_16x16.png", 16, 16)
+local display = spectrum.Display(81, 41, spriteAtlas, prism.Vector2(16, 16))
+
+display:fitWindowToTerminal()
+
 ---@diagnostic disable-next-line: duplicate-set-field
 function love.draw()
-   love.graphics.print("Hello World", 400, 300)
+   local map = prism.Map(81, 41, prism.cells.Wall)
+   local player = prism.actors.Player()
+   player.position = prism.Vector2(12, 12)
+   local actors = { player }
+   local systems = {}
+   local level = prism.Level(map, actors, systems)
+   local levelState = spectrum.LevelState(level, display)
+   levelState.display:clear()
+   levelState.display:putString(1, 1, "Hello World!")
+   levelState.display:draw()
 end
