1d4e7a673779ac8dc1073cacf4ee1587a61a5b73
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-08-01 14:53:58 -0500

    add kicking action

--- modules/game/components/koboldcontroller.lua
+++ modules/game/components/koboldcontroller.lua
@@ -4,9 +4,22 @@ local KoboldController = prism.components.Controller:extend("KoboldController")
 KoboldController.name = "KoboldController"
 
 function KoboldController:act(level, actor)
-   local destination = actor:getPosition() + prism.Vector2.RIGHT
-   local move = prism.actions.Move(actor, destination)
-   if level:canPerform(move) then return move end
+   local senses = actor:get(prism.components.Senses)
+   if not senses then return prism.actions.Wait(actor) end
+
+   local player = senses:query(prism.components.PlayerController):first()
+   if not player then return prism.actions.Wait(actor) end
+
+   local mover = actor:get(prism.components.Mover)
+   if not mover then return prism.actions.Wait(actor) end
+
+   local path = level:findPath(actor:getPosition(), player:getPosition(), actor, mover.mask, 1)
+
+   if path then
+      local move = prism.actions.Move(actor, path:pop())
+      if level:canPerform(move) then return move end
+   end
+
    return prism.actions.Wait(actor)
 end
 
