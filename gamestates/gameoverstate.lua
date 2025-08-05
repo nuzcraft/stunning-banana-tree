df85e68a4ebfc4a1f9789f1eec22cf7d21c5ee50
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-08-04 21:52:41 -0500

    setting up quit and restart commands

--- gamestates/gameoverstate.lua
+++ gamestates/gameoverstate.lua
@@ -1,3 +1,5 @@
+local keybindings = require "keybindingschema"
+
 --- @class GameOverState : GameState
 --- @field display Display
 --- @overload fun(display: Display): GameOverState
@@ -11,7 +13,18 @@ function GameOverState:draw()
    local midpoint = math.floor(self.display.height / 2)
    self.display:clear()
    self.display:putString(1, midpoint, "Game over!", nil, nil, nil, "center", self.display.width)
+   self.display:putString(1, midpoint + 3, "[r] to restart", nil, nil, nil, "center", self.display.width)
+   self.display:putString(1, midpoint + 4, "[q] to quit", nil, nil, nil, "center", self.display.width)
    self.display:draw()
+end
+
+function GameOverState:keypressed(key, scancode, isrepeat)
+   local action = keybindings:keypressed(key, "game-over")
+   if action == "restart" then
+      love.event.restart()
+   elseif action == "quit" then
+      love.event.quit()
+   end
 end
 
 return GameOverState
