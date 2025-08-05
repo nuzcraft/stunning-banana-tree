b6a1399e4c99f62f9a2b550a38d33811d32d2018
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-08-05 13:10:54 -0500

    adding descending actor to descend action and message

--- modules/game/messages/descend.lua
+++ modules/game/messages/descend.lua
@@ -1,4 +1,10 @@
 --- @class DescendMessage : Message
 --- @overload fun(): DescendMessage
 local DescendMessage = prism.Object:extend("DescendMessage")
+
+--- @param descender Actor
+function DescendMessage:__new(descender)
+   self.descender = descender
+end
+
 return DescendMessage
