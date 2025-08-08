65b1259a04704044f3ad9b2e78f1bf2b8b59215b
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-08-08 13:41:49 -0500

    forgot the return on the eat action

--- modules/game/actions/eat.lua
+++ modules/game/actions/eat.lua
@@ -29,3 +29,5 @@ function Eat:perform(level, food)
    Log.addMessage(self.owner, sf("You eat the %s.", Name.get(food)))
    Log.addMessageSensed(level, self, sf("%s eats the %s.", Name.get(self.owner), Name.get(food)))
 end
+
+return Eat
