bf4524764c23a4924e2a36a2a3a6792a5e0e433a
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-08-08 13:41:29 -0500

    adding eat action

---
+++ modules/game/actions/eat.lua
@@ -0,0 +1,31 @@
+local Log = prism.components.Log
+local Name = prism.components.Name
+local sf = string.format
+
+local EatTarget = prism.InventoryTarget(prism.components.Edible):inInventory()
+
+--- @class Eat : Action
+--- @overload fun(owner: Actor, food: Actor): Eat
+local Eat = prism.Action:extend("Eat")
+
+Eat.requiredComponents = {
+   prism.components.Health,
+}
+
+Eat.targets = {
+   EatTarget,
+}
+
+--- @param level Level
+--- @param food Actor
+function Eat:perform(level, food)
+   local edible = food:expect(prism.components.Edible)
+   local health = self.owner:expect(prism.components.Health)
+   health:heal(edible.healing)
+
+   local inventory = self.owner:expect(prism.components.Inventory)
+   inventory:removeQuantity(food, 1)
+
+   Log.addMessage(self.owner, sf("You eat the %s.", Name.get(food)))
+   Log.addMessageSensed(level, self, sf("%s eats the %s.", Name.get(self.owner), Name.get(food)))
+end
