e8e9a6b1f0ee60fd1c5eeb1308a0c399cfb1f73f
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-08-08 21:25:48 -0500

    adding open container action

---
+++ modules/game/actions/opencontainer.lua
@@ -0,0 +1,29 @@
+local Log = prism.components.Log
+local Name = prism.components.Name
+local sf = string.format
+
+local OpenContainerTarget = prism.Target():with(prism.components.Container):range(1):sensed()
+
+--- @class OpenContainer : Action
+local OpenContainer = prism.Action:extend "OpenContainer"
+OpenContainer.targets = { OpenContainerTarget }
+OpenContainer.name = "Open"
+
+--- @param level Level
+--- @param container Actor
+function OpenContainer:perform(level, container)
+   local inventory = container:expect(prism.components.Inventory)
+   local items = inventory:query():gather()
+
+   local x, y = container:expectPosition():decompose()
+   for _, item in ipairs(items) do
+      inventory:removeItem(item)
+      level:addActor(item, x, y)
+   end
+   level:removeActor(container)
+   local containerName = Name.get(container)
+   Log.addMessage(self.owner, sf("You open the %s.", containerName))
+   Log.addMessageSensed(level, self, sf("The %s opens the %s.", Name.get(self.owner), containerName))
+end
+
+return OpenContainer
