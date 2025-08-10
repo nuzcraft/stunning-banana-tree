75ad0a367a6defbb251b7e78bfdbab5ad788f31a
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-08-10 13:51:04 -0500

    adding support for health modifiers to health component

--- modules/game/statusinstance.lua
+++ modules/game/statusinstance.lua
@@ -1,13 +1,13 @@
 --- @class GameStatusInstance : StatusEffectsInstance
 --- @field duration integer?
-local GameStatusInstance = prism.components.StatusEffectsInstance:extend "GameStatusInstance"
+local GameStatusInstance = prism.components.StatusEffects.Instance:extend "GameStatusInstance"
 
 --- @class GameStatusInstanceOptions : StatusEffectsInstanceOptions
 --- @field duration integer
 
 --- @param options GameStatusInstanceOptions
 function GameStatusInstance:__new(options)
-   prism.components.StatusEffectsInstance.__new(self, options)
+   prism.components.StatusEffects.Instance.__new(self, options)
    self.duration = options.duration or nil
 end
 
