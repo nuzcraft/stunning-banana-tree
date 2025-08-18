cbcb756f5594b118d809f33d1b92b4a1d6d05799
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-08-17 20:15:49 -0500

    finishing hurtzappable

--- modules/game/components/zappable/hurtzappable.lua
+++ modules/game/components/zappable/hurtzappable.lua
@@ -4,3 +4,11 @@
 --- @class HurtZappable : Zappable
 --- @overload fun(options: HurtZappableOptions) : HurtZappable
 local HurtZappable = prism.components.Zappable:extend "HurtZappable"
+
+--- @param options HurtZappableOptions
+function HurtZappable:__new(options)
+   prism.components.Zappable.__new(self, options)
+   self.damage = options.damage
+end
+
+return HurtZappable
