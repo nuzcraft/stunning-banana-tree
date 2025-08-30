a0f7c54ec41576e9a56240c23718aa97d87aefa9
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-08-29 21:39:25 -0500

    adding bonusDamage to kicker component

--- modules/game/actors/player.lua
+++ modules/game/actors/player.lua
@@ -19,7 +19,7 @@ prism.registerActor("Player", function()
       },
       prism.components.StatusEffects(),
       prism.components.Attacker(1),
-      prism.components.Kicker(3),
+      prism.components.Kicker(3, 0),
       prism.components.XPCollector(),
    }
 end)
